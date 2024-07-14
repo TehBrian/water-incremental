da45413fc77f9a800fe59016b225b598b89278c9
TehBrian <32250137+TehBrian@users.noreply.github.com>
2024-07-14 17:29:28 -0400

    deploy to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,20 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
+  // https://kit.svelte.dev/docs/integrations#preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "404.html",
       precompress: false,
       strict: true,
     }),
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    },
   },
 };
 
