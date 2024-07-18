cce386b946063e5922f27149018130dc2c81f27e
TehBrian <tehbrian@proton.me>
2024-07-18 19:50:14 -0400

    use :global() to fix VSCode syntax highlighting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,25 @@
 	import LoadingScreen from '$lib/LoadingScreen.svelte';
 	import Stage1 from '$lib/Stage1.svelte';
 	import Header from '$lib/Header.svelte';
-	import { rectifySave, type Save, blankSave, readSaveFromLocalStorage, writeSaveToLocalStorage, updateSave } from '$lib/game-save';
+	import {
+		type Save,
+		rectifySave,
+		blankSave,
+		readSaveFromLocalStorage,
+		writeSaveToLocalStorage,
+		updateSave
+	} from '$lib/game-save';
 
 	let loading = $state(true);
 	let stage1: Stage1;
 
-    function load(save: Save): void {
-        stage1.importSave(rectifySave(save));
-    }
+	function load(save: Save): void {
+		stage1.importSave(rectifySave(save));
+	}
 
-    function save(): Save {
-        return updateSave(blankSave(), stage1.exportSave());
-    }
+	function save(): Save {
+		return updateSave(blankSave(), stage1.exportSave());
+	}
 
 	onMount(async () => {
 		load(readSaveFromLocalStorage());
@@ -33,76 +40,72 @@
 
 <Header {load} {save} />
 
-<div class="stage1">
+<div>
 	<Stage1 bind:this={stage1} />
 </div>
 
-<svelte:head>
-	<style>
-		html {
-			background-color: hsl(250, 10%, 12%);
-			color: #fff;
-		}
-
-		button {
-			padding: 0.3rem;
-			margin: 0.2rem;
-
-			touch-action: manipulation;
-
-			border: 0.1rem solid;
-			border-radius: 0.2rem;
-
-			background-color: hsl(250, 10%, 20%);
-			border-color: hsl(250, 10%, 40%);
-			color: hsl(250, 10%, 100%);
-
-			&:hover {
-				background-color: hsl(250, 10%, 36%);
-				border-color: hsl(250, 10%, 72%);
-			}
-
-			&:active {
-				background-color: hsl(250, 10%, 42%);
-				border-color: hsl(250, 10%, 86%);
-			}
-
-			&:disabled {
-				background-color: hsl(250, 10%, 16%);
-				border-color: hsl(250, 10%, 24%);
-                color: hsl(250, 5%, 50%);
-			}
-		}
-
-		input {
-			padding: 0.3rem;
-			margin: 0.2rem;
-
-			border: 0.1rem solid;
-			border-radius: 0.2rem;
-
-			background-color: hsl(250, 10%, 16%);
-			border-color: hsl(250, 10%, 40%);
-			color: #fff;
-
-			&:hover {
-				border-color: hsl(250, 10%, 52%);
-			}
-
-			&:disabled {
-				border-color: hsl(250, 10%, 24%);
-			}
-		}
-
-        hr {
-			border: 0.1rem solid;
-			border-radius: 0.2rem;
-            border-color: hsl(250, 10%, 36%) hsl(250, 10%, 24%) hsl(250, 10%, 24%);
-        }
-	</style>
-</svelte:head>
-
 <style>
+	:global(html) {
+		background-color: hsl(250, 10%, 12%);
+		color: #fff;
+	}
+
+	:global(button) {
+		padding: 0.3rem;
+		margin: 0.2rem;
+
+		touch-action: manipulation;
+
+		border: 0.1rem solid;
+		border-radius: 0.2rem;
+
+		background-color: hsl(250, 10%, 20%);
+		border-color: hsl(250, 10%, 40%);
+		color: hsl(250, 10%, 100%);
+
+		&:hover {
+			background-color: hsl(250, 10%, 36%);
+			border-color: hsl(250, 10%, 72%);
+		}
+
+		&:active {
+			background-color: hsl(250, 10%, 42%);
+			border-color: hsl(250, 10%, 86%);
+		}
+
+		&:disabled {
+			background-color: hsl(250, 10%, 16%);
+			border-color: hsl(250, 10%, 24%);
+			color: hsl(250, 5%, 50%);
+		}
+	}
+
+	:global(input) {
+		padding: 0.3rem;
+		margin: 0.2rem;
+
+		border: 0.1rem solid;
+		border-radius: 0.2rem;
+
+		background-color: hsl(250, 10%, 16%);
+		border-color: hsl(250, 10%, 40%);
+		color: #fff;
+
+		&:hover {
+			border-color: hsl(250, 10%, 52%);
+		}
+
+		&:disabled {
+			border-color: hsl(250, 10%, 24%);
+		}
+	}
+
+	:global(hr) {
+		border: 0.1rem solid;
+		border-radius: 0.2rem;
+		border-color: hsl(250, 10%, 36%) hsl(250, 10%, 24%) hsl(250, 10%, 24%);
+	}
+
 	div {
 		margin: 0 auto;
 		max-width: 50rem;
